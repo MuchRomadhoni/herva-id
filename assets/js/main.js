506cf85eaf1fabc5f2b31397706e32b5f0e357bc
Dhoni <55375925+MuchRomadhoni@users.noreply.github.com>
2022-06-12 16:26:40 +0700

    + produk dan edit footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -94,11 +94,11 @@
   let backtotop = select(".back-to-top");
   if (backtotop) {
     const toggleBacktotop = () => {
-      if (window.scrollY > 100) {
-        backtotop.classList.add("active");
-      } else {
-        backtotop.classList.remove("active");
-      }
+      // if (window.scrollY > 100) {
+      backtotop.classList.add("active");
+      // } else {
+      //   backtotop.classList.remove("active");
+      // }
     };
     window.addEventListener("load", toggleBacktotop);
     onscroll(document, toggleBacktotop);
